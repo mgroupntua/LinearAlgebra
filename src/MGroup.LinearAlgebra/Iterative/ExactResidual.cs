0930644f542a602559e0b4b3dbb31ecab8485bac
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2021-07-30 19:51:38 +0300

    Increased visibility of a utility class

--- src/MGroup.LinearAlgebra/Iterative/ExactResidual.cs
+++ src/MGroup.LinearAlgebra/Iterative/ExactResidual.cs
@@ -1,22 +1,22 @@
-﻿using MGroup.LinearAlgebra.Vectors;
+using MGroup.LinearAlgebra.Vectors;
 
 namespace MGroup.LinearAlgebra.Iterative
 {
-    internal static class ExactResidual
-    {
-        internal static IVector Calculate(ILinearTransformation matrix, IVectorView rhs, IVectorView solution)
-        {
-            IVector residual = rhs.CreateZeroVectorWithSameFormat();
-            Calculate(matrix, rhs, solution, residual);
-            return residual;
-        }
+	public static class ExactResidual
+	{
+		public static IVector Calculate(ILinearTransformation matrix, IVectorView rhs, IVectorView solution)
+		{
+			IVector residual = rhs.CreateZeroVectorWithSameFormat();
+			Calculate(matrix, rhs, solution, residual);
+			return residual;
+		}
 
-        internal static void Calculate(ILinearTransformation matrix, IVectorView rhs, IVectorView solution, IVector residual)
-        {
-            //TODO: There is a BLAS operation y = y + a * A*x, that would be perfect for here. rhs.Copy() and then that.
-            matrix.Multiply(solution, residual);
-            residual.LinearCombinationIntoThis(-1.0, rhs, 1.0);
-        }
+		public static void Calculate(ILinearTransformation matrix, IVectorView rhs, IVectorView solution, IVector residual)
+		{
+			//TODO: There is a BLAS operation y = y + a * A*x, that would be perfect for here. rhs.Copy() and then that.
+			matrix.Multiply(solution, residual);
+			residual.LinearCombinationIntoThis(-1.0, rhs, 1.0);
+		}
 
-    }
+	}
 }
