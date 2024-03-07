5caefb0eec1eaa7055a63ba5ecf53ec2193c5856
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2024-03-07 15:21:11 +0200

    Updated distributed project and tests for MPI environments

---
+++ tests/MGroup.LinearAlgebra.Distributed.Tests/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MGroup.LinearAlgebra.Distributed.Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Hexagon1DTests.RunMpiTests();
+        }
+    }
+}
